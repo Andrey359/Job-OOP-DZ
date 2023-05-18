da7377e6ccd92c677f7e33ab219630535297c720
Андрей Третьяков <at.tehnol@outlook.com>
2023-05-18 10:57:44 +0300

    Добавьте файлы проекта.

---
+++ 1/ООП/ООП.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+class Drob {
+    float a = 2;
+    float b = 2;
+public:
+    Drob(float a, float b) {
+        this->a = a;
+        this->b = b;
+    }
+    float Add() {
+        return a + b;
+    }
+    float Cat() {
+        return a - b;
+    }
+    float Mul() {
+        return a * b;
+    }
+    float Div() {
+        return a / b;
+    }
+
+};
+
+int main()
+{
+    Drob f(2, 2);
+    cout << f.Mul();
+}
