76215ee79c6a3f7a74cd20b69d7cb79353d7345d
Андрей Третьяков <at.tehnol@outlook.com>
2023-06-14 16:28:56 +0300

    дз

---
+++ linkedlist/linkedlist/linkedlist.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template<typename T, typename U>
+int indexArr(T arr, U poisk) {
+    for (int i = 0; arr[i]; i++) {
+        if (arr[i] == poisk) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = { 1,2,3 };
+    int poisk = 3;
+    cout << indexArr(arr, poisk) << endl;
+
+    char arr2[] = "123";
+    char poisk2 = '4';
+    cout << indexArr(arr2, poisk2) << endl;
+
+    return 0;
+}
---
+++ vector/vector/vector.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+    int x;
+    cout << "Введите количество слов: ";
+    cin >> x;
+    vector<string>words;
+    for (int i = 0; i < x; i++) {
+        string word;
+        cout << "[" << i << "]: ";
+        cin >> word;
+        words.push_back(word);
+    }
+    cout << endl;
+    for (int i = words.size() - 1; i >= 0; --i) {
+        cout << "[" << i << "]: " << words[i] << endl;
+    }
+
+}
