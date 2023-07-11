d3a516b99bc5009f1b1e90ef6e6f7b57c6e05504
Андрей Третьяков <at.tehnol@outlook.com>
2023-07-11 15:33:45 +0300

    getline

---
+++ getline/getline/getline.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    string text;
+    cout << "Введите текст: ";
+    getline(cin, text);
+    cout << "Ваш текст: ";
+    cout << text;
+}
---
+++ static/static/static.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+class User {
+public:
+    string name;
+    static User* lastUser;
+
+    User(const string &name) {
+        this->name = name;
+        lastUser = this;
+    }
+};
+User* User::lastUser = nullptr;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    User one("one");
+    cout << one.name << " -> " << one.lastUser << endl;
+    User two("two");
+    cout << two.name << " -> " << one.lastUser << endl;
+
+}
